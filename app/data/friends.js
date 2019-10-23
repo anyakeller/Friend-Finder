2535c1b8d69c01831a88059106f3cd9f082fa49a
Anya L Keller <kelleranyal@gmail.com>
2019-10-22 21:34:05 -0400

    im dumb

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsArray = [
 {
-  "name":"Ahmed",
+  "name":"YOLO",
   "photo":"https://www.alimentarium.org/en/system/files/thumbnails/image/AL027-01_pomme_de_terre_0.jpg",
   "scores":[
       5,
@@ -12,6 +12,54 @@ var friendsArray = [
       2,
       5,
       4,
+      1
+    ]
+},
+{
+  "name":"Not Yolo",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/9/93/BakedPotatoWithButter.jpg",
+  "scores":[
+      3,
+      4,
+      5,
+      2,
+      1,
+      1,
+      5,
+      2,
+      4,
+      5
+    ]
+},
+{
+  "name":"Also Not Yolo",
+  "photo":"https://www.alimentarium.org/en/system/files/thumbnails/image/AL027-01_pomme_de_terre_0.jpg",
+  "scores":[
+      1,
+      5,
+      5,
+      4,
+      3,
+      5,
+      2,
+      3,
+      2,
+      2
+    ]
+},
+{
+  "name":"Lame Person Who Only Put 1",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
       1
     ]
 }
